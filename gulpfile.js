74801c067b86c4ffe83ff12439b00702ce9dfedf
Danny Kim <dan.k.dev01@gmail.com>
2018-04-09 00:53:04 -0400

    added bootstrap links in html head and implemented nav bar

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,7 @@ gulp.task('sass', function() {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/stylesheets'))
 });
+
+gulp.task('watch', function() {
+  gulp.watch('src/stylesheets/scss/*.scss', ['sass']);
+});
