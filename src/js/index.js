fcabb30cf7a3c5e745ecec92e4f3eef43740cb91
Danny Kim <dan.k.dev01@gmail.com>
2018-05-25 01:41:31 -0400

    fixing dependency issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 (function() {
-  
+  var menuLinks = document.getElementById('#menu-links-container');
+  var body = document.body;
+  function getWindowSize(size) {
+    if (size.matches) {
+      body.removeChild(menuLinks);
+    } else {
+      console.log("Not OK");
+    }
+  }
+
+  var size = window.matchMedia("(min-width: 991px)");
+  getWindowSize(size);
+  size.addListener(getWindowSize);
+
 })()
